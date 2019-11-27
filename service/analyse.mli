0fe1b790b1f3ff2ccc11fd64bc9fb3d9cf39f4e3
Jules Aguillon <juloo.dsi@gmail.com>
2019-11-27 19:00:41 +0100

    Detect dune-projects in the source tree

--- service/analyse.mli
+++ service/analyse.mli
@@ -1,9 +1,16 @@
 module Analysis : sig
+  (** A project is a directory with a dune-project file in it *)
+  type project = {
+    project_path : string;
+    project_name : string option; (** The (name ..) field *)
+  }
+
   type t
 
   val opam_files : t -> string list
   val is_duniverse : t -> bool
   val ocamlformat_version : t -> string option
+  val projects : t -> project list
 end
 
 val examine : Current_git.Commit.t Current.t -> Analysis.t Current.t
